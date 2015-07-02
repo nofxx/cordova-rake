ecebc55aabbd669528ecce2d28244639bdb43f83
Marcos Piccinini <x@nofxx.com>
2015-07-02 03:21:06 -0300

    organize a bit

--- lib/cordova/rake.rb
+++ lib/cordova/rake.rb
@@ -4,6 +4,40 @@ require 'tilt'
 require 'nokogiri'
 START = Time.now
 
-# Just load all tasks
+#
+# Some helper methods
+#
+def get_sources(ext, dir = 'app')
+  source_files = Rake::FileList.new("#{dir}/**/*.#{ext}") do |fl|
+    fl.exclude("~*")
+    fl.exclude(/^scratch\//)
+    # fl.exclude { |f| `git ls-files #{f}`.empty? } # Only commited
+  end
+end
+
+def environment
+  ENV['TARGET'] || 'development'
+end
+
+def config(key)
+  return @xml[key] if @xml
+  xml = Nokogiri::XML(File.open('config.xml'))
+  @xml = {
+    app: xml.xpath("//xmlns:name").text,
+    desc: xml.xpath("//xmlns:description").text,
+    platforms: xml.xpath("//xmlns:platform").map { |os| os['name'] }
+  }
+  config(key)
+end
+
+def layout
+  @layout ||= Tilt.new('app/html/layout.haml')
+end
+
+def app
+  config(:app)
+end
+
+# And load all tasks
 tasks = File.join(File.dirname(__FILE__), '..', 'tasks')
 Dir.glob("#{tasks}/*.rake").each { |r| import r }
--- lib/tasks/cordova.rake
+++ lib/tasks/cordova.rake
@@ -1,36 +1,6 @@
 #
 # Rake / Cordova
 #
-def get_sources(ext, dir = 'app')
-  source_files = Rake::FileList.new("#{dir}/**/*.#{ext}") do |fl|
-    fl.exclude("~*")
-    fl.exclude(/^scratch\//)
-    # fl.exclude { |f| `git ls-files #{f}`.empty? } # Only commited
-  end
-end
-
-def environment
-  ENV['TARGET'] || 'development'
-end
-
-def config(key)
-  return @xml[key] if @xml
-  xml = Nokogiri::XML(File.open('config.xml'))
-  @xml = {
-    app: xml.xpath("//xmlns:name").text,
-    desc: xml.xpath("//xmlns:description").text,
-    platforms: xml.xpath("//xmlns:platform").map { |os| os['name'] }
-  }
-  config(key)
-end
-
-def app
-  config(:app)
-end
-
-def layout
-  @layout ||= Tilt.new('app/html/layout.haml')
-end
 
 task default: [:greet, :compile, :report]
 
